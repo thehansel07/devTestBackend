1946b02127a35a3d11eb551ffe74c9a622fcbaf8
thehansel07 <hanselcabreraespinal@gmail.com>
2024-01-05 18:52:28 -0400

    Fixing issues with scripts and layers

--- DevTestBackend.Domain/Entities/Announcement.cs
+++ DevTestBackend.Domain/Entities/Announcement.cs
@@ -9,11 +9,11 @@ namespace DevTestBackend.Domain.Entities
         public string title { get; set; }
         public string content { get; set; }
         public DateTime date { get; set; }
-        public DateTime AuditCreateUser { get; set; }
+        public int AuditCreateUser { get; set; }
         public DateTime AuditCreateDate { get; set; }
-        public DateTime AuditUpdateUser { get; set; }
+        public int AuditUpdateUser { get; set; }
         public DateTime AuditUpdateDate { get; set; }
-        public DateTime AuditDeleteUser { get; set; }
+        public int AuditDeleteUser { get; set; }
         public DateTime AuditDeleteDate { get; set; }
     }
 }
